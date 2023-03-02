25697258184c4126784525c5d1d5cfd6a67c600c
Chinseok Lee <me@pyhub.kr>
2023-03-02 21:41:21 +0900

    mall 앱 Category/Post 모델 추가 및 마이그레이션 등록

--- mall/admin.py
+++ mall/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = "상품 분류"
+
+
+class Product(models.Model):
+    class Status(models.TextChoices):
+        ACTIVE = "a", "정상"
+        SOLD_OUT = "s", "품절"
+        OBSOLETE = "o", "단종"
+        INACTIVE = "i", "비활성화"
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    name = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    price = models.PositiveIntegerField()  # 0 포함
+    status = models.CharField(
+        choices=Status.choices, default=Status.INACTIVE, max_length=1
+    )
+    photo = models.ImageField(
+        upload_to="mall/product/photo/%Y/%m/%d",
+    )
+
+    def __str__(self):
+        return f"<{self.pk}> {self.name}"
+
+    class Meta:
+        verbose_name = verbose_name_plural = "상품"
