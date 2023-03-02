1bd9a310368a7a5c1bdf374c8c1c5776887028ef
Chinseok Lee <me@pyhub.kr>
2023-03-02 14:29:11 +0900

    mall_test Payment 모델 정의 및 마이그레이션 추가

--- mall_test/models.py
+++ mall_test/models.py
@@ -1,3 +1,33 @@
+from uuid import uuid4
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Payment(models.Model):
+    class StatusChoices(models.TextChoices):
+        READY = "ready", "미결제"
+        PAID = "paid", "결제완료"
+        CANCELLED = "cancelled", "결제취소"
+        FAILED = "failed", "결제실패"
+
+    uid = models.UUIDField(default=uuid4, editable=False)
+    name = models.CharField(max_length=100)
+    amount = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1, message="1원 이상의 금액을 지정해주세요."),
+        ]
+    )
+    status = models.CharField(
+        max_length=9,
+        default=StatusChoices.READY,
+        choices=StatusChoices.choices,
+        db_index=True,
+    )
+    is_paid_ok = models.BooleanField(default=False, db_index=True)
+
+    @property
+    def merchant_uid(self) -> str:
+        return self.uid.hex
+
+    # TODO: 포트원 REST API를 통해서 결제를 검증해야만 합니다.
