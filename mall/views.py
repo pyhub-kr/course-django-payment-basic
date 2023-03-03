2a0c2ca4c3baac1470ce29c7867a43c1609f65e4
Chinseok Lee <me@pyhub.kr>
2023-03-03 13:51:03 +0900

    상품목록 뷰에서 검색어를 쿼리셋에 반영

--- mall/views.py
+++ mall/views.py
@@ -8,5 +8,14 @@ class ProductListView(ListView):
     queryset = Product.objects.all().select_related("category")
     paginate_by = 4
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        query = self.request.GET.get("query", "")
+        if query:
+            qs = qs.filter(name__icontains=query)
+
+        return qs
+
 
 product_list = ProductListView.as_view()
