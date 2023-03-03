d83eba4d26e7be08871a309084f0c3e8c8f5eaad
Chinseok Lee <me@pyhub.kr>
2023-03-03 16:02:16 +0900

    자바스크립트를 활용한 장바구니 담기

--- mall/views.py
+++ mall/views.py
@@ -1,7 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.forms import modelformset_factory
+from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_POST
 from django.views.generic import ListView
 
 from mall.forms import CartProductForm
@@ -69,6 +71,7 @@ def cart_detail(request):
 
 
 @login_required
+@require_POST
 def add_to_cart(request, product_pk):
     product_qs = Product.objects.filter(
         status=Product.Status.ACTIVE,
@@ -86,8 +89,9 @@ def add_to_cart(request, product_pk):
         cart_product.quantity += quantity
         cart_product.save()
 
-    messages.success(request, "장바구니에 추가했습니다.")
+    # messages.success(request, "장바구니에 추가했습니다.")
 
-    redirect_url = request.META.get("HTTP_REFERER", "product_list")
+    # redirect_url = request.META.get("HTTP_REFERER", "product_list")
+    # return redirect(redirect_url)
 
-    return redirect(redirect_url)
+    return HttpResponse("ok")
