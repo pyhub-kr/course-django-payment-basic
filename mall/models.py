930b046c7dad2a0ee1e390463f7f8e2fd9f3f7b3
Chinseok Lee <me@pyhub.kr>
2023-03-03 16:43:45 +0900

    주문 모델 설계

--- mall/models.py
+++ mall/models.py
@@ -1,3 +1,5 @@
+from uuid import uuid4
+
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.db.models import UniqueConstraint
@@ -74,3 +76,54 @@ class CartProduct(models.Model):
         constraints = [
             UniqueConstraint(fields=["user", "product"], name="unique_user_product"),
         ]
+
+
+class Order(models.Model):
+    class Status(models.TextChoices):
+        REQUESTED = "requested", "주문요청"
+        FAILED_PAYMENT = "failed_payment", "결제실패"
+        PAID = "paid", "결제완료"
+        PREPARED_PRODUCT = "prepared_product", "상품준비중"
+        SHIPPED = "shipped", "배송중"
+        DELIVERED = "delivered", "배송완료"
+        CANCELED = "canceled", "주문취소"
+
+    uid = models.UUIDField(default=uuid4, editable=False)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    total_amount = models.PositiveIntegerField("결제금액")
+    status = models.CharField(
+        "진행상태",
+        max_length=20,
+        choices=Status.choices,
+        default=Status.REQUESTED,
+        db_index=True,
+    )
+    product_set = models.ManyToManyField(
+        Product,
+        through="OrderedProduct",
+        blank=False,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class OrderedProduct(models.Model):
+    order = models.ForeignKey(
+        Order,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+        db_constraint=False,
+    )
+    name = models.CharField("상품명", max_length=100, help_text="주문 시점의 상품명을 저장합니다.")
+    price = models.PositiveIntegerField("상품가격", help_text="주문 시점의 상품가격을 저장합니다.")
+    quantity = models.PositiveIntegerField("수량")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
