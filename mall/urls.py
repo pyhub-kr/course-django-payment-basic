e65b978d9563ea4814db84181ebfdeecb35901de
Chinseok Lee <me@pyhub.kr>
2023-03-03 10:47:07 +0900

    상품 페이지 만들기

--- mall/urls.py
+++ mall/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.product_list, name="product_list"),
+]
--- mall/views.py
+++ mall/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from mall.models import Product
+
+
+def product_list(request):
+    product_qs = Product.objects.all().select_related("category")
+    return render(
+        request,
+        "mall/product_list.html",
+        {
+            "product_list": product_qs,
+        },
+    )
