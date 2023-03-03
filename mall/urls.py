59a0dff85efedde4f65d5e5f91a079b3b96c2e86
Chinseok Lee <me@pyhub.kr>
2023-03-03 14:29:03 +0900

    장바구니 담기 뷰 추가

--- mall/urls.py
+++ mall/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.product_list, name="product_list"),
+    path("cart/<int:product_pk>/add/", views.add_to_cart, name="add_to_cart"),
 ]
--- mall/views.py
+++ mall/views.py
@@ -1,11 +1,16 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404, redirect
 from django.views.generic import ListView
 
-from mall.models import Product
+from mall.models import Product, CartProduct
 
 
 class ProductListView(ListView):
     model = Product
-    queryset = Product.objects.all().select_related("category")
+    queryset = Product.objects.filter(status=Product.Status.ACTIVE).select_related(
+        "category"
+    )
     paginate_by = 4
 
     def get_queryset(self):
@@ -19,3 +24,26 @@ class ProductListView(ListView):
 
 
 product_list = ProductListView.as_view()
+
+
+@login_required
+def add_to_cart(request, product_pk):
+    product_qs = Product.objects.filter(
+        status=Product.Status.ACTIVE,
+    )
+    product = get_object_or_404(product_qs, pk=product_pk)
+
+    quantity = int(request.GET.get("quantity", 1))
+
+    cart_product, is_created = CartProduct.objects.get_or_create(
+        user=request.user,
+        product=product,
+        defaults={"quantity": quantity},
+    )
+    if not is_created:
+        cart_product.quantity += quantity
+        cart_product.save()
+
+    messages.success(request, "장바구니에 추가했습니다.")
+
+    return redirect("product_list")
